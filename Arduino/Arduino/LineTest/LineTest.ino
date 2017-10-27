1894cf7e0922b5cfd9d0c3275a74bfe7f462ce13
amanjyot <amanjyot2911@gmail.com>
2017-10-27 15:29:06 -0500

    Line following with Stutter Correction
    Changed code to only do position correction if middle two sensors do not detect the line or if any of the four end sensors detect a line.

--- Arduino/Arduino/LineTest/LineTest.ino
+++ Arduino/Arduino/LineTest/LineTest.ino
@@ -24,6 +24,8 @@ ZumoReflectanceSensorArray reflectanceSensors;
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
 int lastError = 0;
+const int numOfSensors = 6;
+const int minLineFound = 750;
 
 // This is the maximum speed the motors will be allowed to turn.
 // (400 lets the motors go at top speed; decrease to impose a speed limit)
@@ -34,7 +36,7 @@ void setup()
 {
   Serial.begin(9600);
   // Play a little welcome song
-  buzzer.play(">g32>>c32");
+//  buzzer.play(">g32>>c32");
 
   // Initialize the reflectance sensors module
   reflectanceSensors.init();
@@ -66,7 +68,7 @@ void setup()
 
   // Turn off LED to indicate we are through with calibration
   digitalWrite(13, LOW);
-  buzzer.play(">g32>>c32");
+//  buzzer.play(">g32>>c32");
 
   // Wait for the user button to be pressed and released
   button.waitForButton();
@@ -76,52 +78,90 @@ void setup()
 //  while(buzzer.isPlaying());
 }
 
+// This method takes in the value from the 6 sensors, then creates a
+// boolean array with value true if that sensor found the line otherwise
+// inputs value false. It then sets the isCentered boolean variable to 
+// true only if either the two middle sensors detected and the number of
+// lines detected is less
+boolean centered(unsigned int sensors[numOfSensors]){
+  int onLineCount = 0;
+  int sensorCount = 0;
+  boolean isCentered;
+  boolean sensorsOnLine[numOfSensors];
+  
+  for (int sensorCount=0; sensorCount < numOfSensors; sensorCount++) {
+    if (sensors[sensorCount] >= minLineFound){
+      sensorsOnLine[sensorCount] = true;
+      onLineCount++;
+    } else {
+      sensorsOnLine[sensorCount] = false;
+    }
+  }
+
+  if (onLineCount < 3 and (sensorsOnLine[2] == true or sensorsOnLine[3] == true)){
+    isCentered = true;
+  } else {
+    isCentered = false;
+  }
+
+//  Serial.print(" "); Serial.print(isCentered); Serial.print(" | ");
+//  for(int i=0; i< 6; i++) {
+//    Serial.print(sensors[i]);
+//    Serial.print(",");
+//  }
+//  Serial.println();
+
+  return isCentered;
+}
+
 void loop()
 {
-  unsigned int sensors[6];
+  unsigned int sensors[numOfSensors];
 
   // Get the position of the line.  Note that we *must* provide the "sensors"
   // argument to readLine() here, even though we are not interested in the
   // individual sensor readings
   int position = reflectanceSensors.readLine(sensors);
-  for(int i=0; i< 6; i++) {
-    Serial.print(sensors[i]);
-    Serial.print(",");
+//  Serial.print(position);
+
+  // ONLY correct direction if the line is not detected from one of the middle
+  // sensors or if the two sensors on both end detect a line.
+  if (!centered(sensors)) {
+    // Our "error" is how far we are away from the center of the line, which
+    // corresponds to position 2500.
+    int error = position - 2500;
+  
+    // Get motor speed difference using proportional and derivative PID terms
+    // (the integral term is generally not very useful for line following).
+    // Here we are using a proportional constant of 1/4 and a derivative
+    // constant of 6, which should work decently for many Zumo motor choices.
+    // You probably want to use trial and error to tune these constants for
+    // your particular Zumo and line course.
+    int speedDifference = error / 4 + 6 * (error - lastError);
+  
+    lastError = error;
+  
+    // Get individual motor speeds.  The sign of speedDifference
+    // determines if the robot turns left or right.
+    int m1Speed = MAX_SPEED + speedDifference;
+    int m2Speed = MAX_SPEED - speedDifference;
+  
+    // Here we constrain our motor speeds to be between 0 and MAX_SPEED.
+    // Generally speaking, one motor will always be turning at MAX_SPEED
+    // and the other will be at MAX_SPEED-|speedDifference| if that is positive,
+    // else it will be stationary.  For some applications, you might want to
+    // allow the motor speed to go negative so that it can spin in reverse.
+    if (m1Speed < 0)
+      m1Speed = 0;
+    if (m2Speed < 0)
+      m2Speed = 0;
+    if (m1Speed > MAX_SPEED)
+      m1Speed = MAX_SPEED;
+    if (m2Speed > MAX_SPEED)
+      m2Speed = MAX_SPEED;
+  
+    motors.setSpeeds(m1Speed, m2Speed);
+  } else {
+    motors.setSpeeds(MAX_SPEED,MAX_SPEED);
   }
-  Serial.println();
-
-  // Our "error" is how far we are away from the center of the line, which
-  // corresponds to position 2500.
-  int error = position - 2500;
-
-  // Get motor speed difference using proportional and derivative PID terms
-  // (the integral term is generally not very useful for line following).
-  // Here we are using a proportional constant of 1/4 and a derivative
-  // constant of 6, which should work decently for many Zumo motor choices.
-  // You probably want to use trial and error to tune these constants for
-  // your particular Zumo and line course.
-  int speedDifference = error / 4 + 6 * (error - lastError);
-
-  lastError = error;
-
-  // Get individual motor speeds.  The sign of speedDifference
-  // determines if the robot turns left or right.
-  int m1Speed = MAX_SPEED + speedDifference;
-  int m2Speed = MAX_SPEED - speedDifference;
-
-  // Here we constrain our motor speeds to be between 0 and MAX_SPEED.
-  // Generally speaking, one motor will always be turning at MAX_SPEED
-  // and the other will be at MAX_SPEED-|speedDifference| if that is positive,
-  // else it will be stationary.  For some applications, you might want to
-  // allow the motor speed to go negative so that it can spin in reverse.
-  if (m1Speed < 0)
-    m1Speed = 0;
-  if (m2Speed < 0)
-    m2Speed = 0;
-  if (m1Speed > MAX_SPEED)
-    m1Speed = MAX_SPEED;
-  if (m2Speed > MAX_SPEED)
-    m2Speed = MAX_SPEED;
-
-  motors.setSpeeds(m1Speed, m2Speed);
 }
